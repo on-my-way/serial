f37e37fa1fb6692e6845dbe45f43038a0dfec4cc
youpan <wyp_342201@163.com>
2017-10-17 23:12:39 +0800

    base

--- python_flask.py
+++ python_flask.py
@@ -18,7 +18,10 @@ import json
 import serial
 import serial.tools.list_ports
 
-global ser
+import queue
+
+
+serial_isOpen = False 
 
 @app.route('/get_coms', methods=['GET'])
 def get_coms():
@@ -32,6 +35,7 @@ def get_coms():
     
 @app.route('/get_bauds', methods=['GET'])
 def get_bauds():
+    global ser
     ser = serial.Serial(request.args.get("port"))
     coms = {"bauds": (9600)}
     coms['bauds'] = ser.BAUDRATES
@@ -39,24 +43,36 @@ def get_bauds():
 
 @app.route('/set_baud', methods=['POST'])
 def set_bauds():
+    global ser
     if ser.isOpen():
         print("close serial port")
         ser.close()
     baud = int(request.form.get("baud"))
     ser.baudrate= baud
     ser.open()
-    print("open serier port baud=%" %baud)
+    serial_isOpen = True
+    print("open serier port baud=%d" %baud)
     return ""
 
 
 @app.route('/get_data', methods=['GET'])
 def get_data():
-    while not dq.empty()
-        return ""
-    for 
+    global dq
+    data = ""
+    while not dq.empty():
+        data += dq.get()
+    return data 
 
 
+import time, threading
 
+def loop():
+    global ser
+    global dq
+    if serial_isOpen:
+        d = ser.read(128)
+        dq.put(d)
+    time.sleep(2)
 
 
 
@@ -64,4 +80,11 @@ def get_data():
 
 
 if __name__ == '__main__':
+    global dq
+    dq = queue.Queue()
+    dq.put("bb")
+    t = threading.Thread(target=loop, name='LoopThread')
+    t.start()
+    print("thread start")
     app.run()
+    print("app run")
